af7f567f100a42288f42ec322f535ab08dc399ec
Manuel Betances <manubetances@gmail.com>
2024-02-23 11:11:55 -0500

    Initialize Repository

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const words = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.'.split(' ');
+const wordCount = words.length;
+
+function addClass(el, name) {
+	el.className += ' ' + name;
+}
+
+function removeClass(el, name) {
+	el.className = el.className.replace(name, '');
+}
+
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordCount);
+  return words[randomIndex];
+}
+
+//
+function formatWord(word) {
+  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+}
+
+//
+function newGame() {
+  document.getElementById('textArea').innerHTML = '';
+
+  for (let i = 0; i < 200; i++) {
+    document.getElementById('textArea').innerHTML += formatWord(randomWord());
+  }
+  addClass(document.querySelector('.word'), 'current');
+  addClass(document.querySelector('.letter'), 'current');
+}
+
+document.getElementById('game').addEventListener('keyup', e => {
+	const key = e.key;
+	//
+	const currentLetter = document.querySelector('.letter.current');
+	//
+	const expected = currentLetter.innerHTML();
+});
+
+newGame();
